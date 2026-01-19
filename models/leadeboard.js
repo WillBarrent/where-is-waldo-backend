82597ad79e86f2da82940d75e58bf4cb6cb69b2d
WillBarrent <161952799+WillBarrent@users.noreply.github.com>
2026-01-19 21:32:41 +0500

    Same as controllers: Just add puzzleId to the updatePlayerById function

--- models/leadeboard.js
+++ models/leadeboard.js
@@ -1,19 +1,22 @@
 const { prisma } = require("./db");
 
 const createPlayer = async (username, completionTime, puzzleId) => {
-  await prisma.player.create({
+  const player = await prisma.player.create({
     data: {
       username: username,
       completionTime: Number(completionTime),
       puzzleId: Number(puzzleId),
     },
   });
+
+  return player;
 };
 
-const updatePlayerById = async (playerId, username) => {
+const updatePlayerById = async (playerId, puzzleId, username) => {
   await prisma.player.update({
     where: {
       id: Number(playerId),
+      puzzleId: puzzleId,
     },
     data: {
       username: username,
